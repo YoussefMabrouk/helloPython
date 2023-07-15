991bd2bbfe5b5d9f71ef946a0e60b29cd21dc761
YoussefMabrouk <99642449+YoussefMabrouk@users.noreply.github.com>
2023-07-15 20:36:25 +0200

    Update test_print.py

--- test_print.py
+++ test_print.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 def test_numpy_version():
-    assert np.__version__ == "1.21.0"
+    assert np.__version__ == "1.25.1"
 
 def test_pandas_version():
-    assert pd.__version__ == "1.3.0"
+    assert pd.__version__ == "2.0.3"
