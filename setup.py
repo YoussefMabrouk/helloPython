caf367d7e30865df1cb339edc0049cab43384006
YoussefMabrouk <99642449+YoussefMabrouk@users.noreply.github.com>
2023-07-15 18:45:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="helloworld",
     version="1.0.0",
-    author="YoussefMabrouk",
-    author_email="mabrouk@uni-muenster.de",
-    description="A project that says helloWorld",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple Python project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=[""],
     install_requires=["numpy", "pandas"],
 )
+
