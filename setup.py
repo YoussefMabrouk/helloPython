0ffd852db2719d66514b7be01df50d11dc2ab916
YoussefMabrouk <99642449+YoussefMabrouk@users.noreply.github.com>
2023-07-15 18:41:52 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="helloworld",
+    version="1.0.0",
+    author="YoussefMabrouk",
+    author_email="mabrouk@uni-muenster.de",
+    description="A project that says helloWorld",
+    packages=[""],
+    install_requires=["numpy", "pandas"],
+)
